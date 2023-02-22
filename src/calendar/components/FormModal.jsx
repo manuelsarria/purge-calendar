b86fdb6f885b7a896c9ccf347d5bea7af9858e06
Manuel Sarrria <victor.sarria@utp.ac.pa>
2023-02-22 15:51:55 -0500

    validaciones de los formularios

--- src/calendar/components/FormModal.jsx
+++ src/calendar/components/FormModal.jsx
@@ -1,96 +1,13 @@
-import { addHours, differenceInSeconds } from "date-fns";
-import { useState } from "react";
-import Modal from "react-modal"
-import Swal from "sweetalert2";
-import 'sweetalert2/dist/sweetalert2.min.css';
+
 import DatePicker, { registerLocale } from "react-datepicker";
 import es from 'date-fns/locale/es';
 import "react-datepicker/dist/react-datepicker.css";
 
 registerLocale('es', es);
 
-const customStyles = {
-  content: {
-    top: '50%',
-    left: '50%',
-    right: 'auto',
-    bottom: 'auto',
-    marginRight: '-50%',
-    transform: 'translate(-50%, -50%)',
-  },
-};
-
-Modal.setAppElement('#root');
-
-export const CalendarModal = () => {
-
-  const [isOpen, setIsOpen] = useState(true);
-  const [formSubmitted, setFormSubmitted] = useState(false);
-
-  const [formValues, setFormValues] = useState({
-    title: 'Manuel',
-    notes: 'sarria',
-    start: new Date(),
-    end: addHours( new Date(), 2),
-  });
-
-  const titleClass = useMemo(() => {
-    if ( !formSubmitted ) return '';
-    
-    return ( formValues.title.length > 0)
-    ? 'is-valid'
-    : 'is-invalid'
-  }, [formValues.title, formSubmitted])
-
-  const onInputChanged = ({ target }) => {
-    setFormValues({
-      ...formValues,
-      [target.name]: target.value
-    })
-  }
-
-  const onDateChanged = ( event, changing ) => {
-    setFormValues({
-      ...formValues,
-      [changing]: event
-    })
-  }
-
-  const onCloseModal = () => {
-    console.log('cerrando modal');
-    setIsOpen(false);
-  }
-
-  const onSubmit = ( event ) => {
-    event.preventDefault();
-    setFormSubmitted(true);
-    const difference = differenceInSeconds( formValues.end, formValues.start );
-
-    if ( isNaN( difference ) || difference <= 0) {
-      Swal.fire('Fechas incorrectas', 'Revisar las fechas ingresadas', 'error')
-      return
-    }
-
-    if ( formValues.title.length <= 0 ) return;
-
-    console.log(formValues);
-
-    //TODO
-
-    // cerrar modal
-    // remover errores en pantalla
-
-  }
-  
+export const FormModal = (onSubmit, onInputChanged, onDateChanged, formValues) => {
   return (
-    <Modal
-      isOpen={ isOpen }
-      onRequestClose={ onCloseModal }
-      style={ customStyles }
-      className="modal"
-      overlayClassName="modal-fondo"
-      closeTimeoutMS={ 200 }
-    >
+    <>
       <h1> Nuevo evento </h1>
       <hr />
       <form className="container" onSubmit={ onSubmit }>
@@ -159,6 +76,6 @@ export const CalendarModal = () => {
           </button>
 
       </form>
-    </Modal>
+    </>
   )
 }
--- src/calendar/index.js
+++ src/calendar/index.js
@@ -6,3 +6,4 @@ export * from './pages/CalendarPage';
 export * from './components/Navbar';
 export * from './components/CalendarEvent';
 export * from './components/CalendarModal';
+export * from './components/FormModal';
