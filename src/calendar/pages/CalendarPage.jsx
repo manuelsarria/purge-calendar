2b000a64d774245eb9082346e2c15ef964971a46
Manuel Sarrria <victor.sarria@utp.ac.pa>
2023-02-22 11:44:06 -0500

    first commit

---
+++ src/calendar/pages/CalendarPage.jsx
@@ -0,0 +1,55 @@
+import { Calendar } from 'react-big-calendar';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+import { addHours } from 'date-fns';
+import { Navbar, CalendarEvent } from "../";
+import { getMessagesES, localizer } from '../../helpers';
+
+const events = [{
+  title: 'cumpleanos del jefe',
+  notes: 'hay que comprar el pastel',
+  start: new Date(),
+  end: addHours( new Date(), 2),
+  bgColor: '#fafafa',
+  user: {
+    _id: '123',
+    name: 'Fernando',
+  }
+}]
+
+const eventStyleGetter = ( event, start, end, isSelected ) => {
+  console.log({event, start, end, isSelected});
+
+  const style = {
+    backgroundColor: '#347CF7',
+    borderRadius: '0px',
+    opacity: 0.8,
+    color: 'white',
+  }
+
+  return {
+    style
+  }
+}
+
+export const CalendarPage = () => {
+  return (
+    <>
+      <Navbar />
+
+      <Calendar
+        culture='es'
+        localizer={ localizer }
+        events={ events }
+        startAccessor="start"
+        endAccessor="end"
+        style={{ height: 'calc( 100vh - 80px )' }}
+        messages={ getMessagesES() }
+        eventPropGetter={ eventStyleGetter }
+        components={{
+          event: CalendarEvent
+        }}
+      />
+    </>
+  )
+}
